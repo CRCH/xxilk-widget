097c135465b35f541101f0dd02c879caa73c765b
Andrew Meshcheryakov <cryptonov@gmail.com>
2022-10-11 23:29:58 +0300

    upd name

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { inlineSvg } from 'stencil-inline-svg';
 
 export const config: Config = {
-  namespace: 'xxilk-widget',
+  namespace: 'widget-xxilk',
   outputTargets: [
     {
       type: 'dist',
